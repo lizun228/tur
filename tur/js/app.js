7485e7f7d2b264c6f40c436819403b723cf07da1
lizun228 <152127988+lizun228@users.noreply.github.com>
2023-11-27 06:09:38 +0300

    Delete tur directory

--- tur/js/app.js
+++
@@ -1,8 +0,0 @@
-const MenuBtn = document.getElementById('toggleMenu');
-const ModalWindow = document.querySelector('.ModalWindow');
-
-
-MenuBtn.addEventListener('click', function() {
-    MenuBtn.classList.toggle('open-menu');
-    ModalWindow.classList.toggle('active');
-})
